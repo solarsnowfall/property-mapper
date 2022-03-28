1f4c4fd62a30f83e101b773962e59a2ea6e47a09
solarsnowfall <travis@twooutrally.com>
2022-03-28 14:30:49 -0500

    Minor refactoring...

--- src/Mapper.php
+++ src/Mapper.php
@@ -2,8 +2,46 @@
 
 namespace Solarsnowfall\Property;
 
+use Solarsnowfall\String\Convention;
+
 abstract class Mapper
 {
+    const MAGIC_GETTERS = false;
+
+    const MAGIC_SETTERS = false;
+
+    const PROPERTY_CONVENTION = null;
+
+    /**
+     * @param string $name
+     * @param array $arguments
+     * @return mixed
+     */
+    public function __call(string $name, array $arguments)
+    {
+        $prefix = substr(strtolower($name), 0, 3);
+
+        if ($prefix === 'get' && !static::MAGIC_GETTERS || $prefix === 'set' && !static::MAGIC_SETTERS)
+            trigger_error("Call to undefined method " . get_called_class() . "::{$name}()", E_USER_ERROR);
+
+        $property = ltrim(substr($name, 3), '_');
+
+        $property = Convention::convert($property, static::PROPERTY_CONVENTION);
+
+        if (!property_exists($this, $property))
+            trigger_error("Call to undefined method " . get_called_class() . "::{$name}()", E_USER_ERROR);
+
+        if ($property === 'get')
+            return $this->$property;
+
+        if (!count($arguments))
+            throw new \ArgumentCountError("Too few arguments to function " . get_called_class() . "::{$name}()");
+
+        $this->$property = $arguments[0];
+
+        return $this;
+    }
+
     /**
      * @param int $visibility
      * @return $this
